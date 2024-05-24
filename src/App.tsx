00ad80333c6add9cbea629600a43c95158ec8b4f
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-05-25 00:04:54 +0530

    header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Header from "./components/header/Header"
 
 function App() {
 
   return (
-    <div className='bg-blue-600 w-screen h-screen'>
-      hello
+    <div className='bg-slate-200 w-screen h-screen flex items-center justify-center overflow-y-scroll'>
+      <div className="bg-blue-200 w-11/12 min-h-screen">
+        <Header />
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dancing: ['"Dancing Script"', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
