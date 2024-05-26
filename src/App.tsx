bcc9d291415ac14898f53df90ed941858a91de12
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-05-26 18:50:29 +0530

    nav responsive done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Header from "./components/header/Header"
-import Hero from "./components/header/Hero"
 
 function App() {
 
   return (
     <div className='bg-slate-200 w-screen h-screen flex items-center justify-center overflow-y-scroll'>
-      <div className="w-11/12 min-h-screen transition ease-in-out delay-150">
+      <div className="w-[96%] max min-h-screen lg:w-[720px] xl:w-[960px] 2xl:w-[1140px] 3xl:w-[1320px]">
         <Header />
-        <Hero />
       </div>
     </div>
   )
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 
     return (
         <>
-            <div className="bg-zinc-200 w-full h-20 flex items-center justify-between">
+            <div className="w-full h-20 flex items-center justify-between">
                 <h1 className="font-dancing text-5xl font-extrabold">Tastebite</h1>
                 <nav className="hidden lg:block lg:w-2/4 h-full">
                     <ul className="flex items-center justify-evenly text-base font-medium w-full h-full">
@@ -23,21 +23,23 @@ const Header = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 ms-4 lg:ms-7">
                         <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
                     </svg>
-                    <svg onClick={() => setNav(true)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 ms-4 lg:hidden">
+                    <svg onClick={() => setNav((pre) => !pre)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 ms-4 lg:hidden">
                         <path fillRule="evenodd" d="M3 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 5.25Zm0 4.5A.75.75 0 0 1 3.75 9h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 9.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
                     </svg>
                 </div>
             </div>
-            {nav &&
-                <div className="bg-yellow-50 w-full h-auto">
-                    <ul className="flex flex-col items-start text-base font-medium">
-                        <li>Home Page</li>
-                        <li>Recipe Page</li>
-                        <li>Pages</li>
-                        <li>Elements</li>
-                        <li>Buy</li>
-                    </ul>
-                </div>}
+            <div className={`block w-full h-auto lg:hidden`}>
+                <ul className="flex flex-col items-start text-base font-medium">
+                    <li className="w-full mt-3 mb-3">Home Page</li>
+                    <li className="w-full mt-3 mb-3">Recipe Page</li>
+                    <li className="w-full mt-3 mb-3">Pages</li>
+                    <li className="w-full mt-3 mb-3">Elements</li>
+                    <li className="w-full mt-3 mb-3">Buy</li>
+                </ul>
+            </div>
+            <div className={`w-full h-auto transition ease-in-out duration-500  ${nav && '-translate-y-60'}`}>
+                <img className="w-full h-auto lg:w-[50%] rounded-t-2xl" src="https://fabrx.co/preview/tastebite/assets/images/menus/menu1.jpg" alt="" />
+            </div>
         </>
     )
 }
