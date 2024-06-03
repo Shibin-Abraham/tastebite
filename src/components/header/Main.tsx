2f1235aa1c1f9e3985147f998355ebd1782416d3
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-06-04 00:03:50 +0530

    subscription responsive done

--- src/components/header/Main.tsx
+++ src/components/header/Main.tsx
@@ -25,31 +25,32 @@ const Main = () => {
                     delicious.map((data, index) => {
 
                         return (
-                            <div key={index} className="mt-4 w-full h-[25.5rem] md:w-[30%] md:h-[20rem] md:mr-1 group">
-                                <div className="w-full h-[80%] flex items-center justify-center overflow-hidden rounded-lg">
-                                    <img className="w-full h-full rounded-lg object-cover group-hover:scale-110 duration-700 transition-all" src={data.strMealThumb} alt="" />
-                                </div>
-                                <div className="w-full mt-2">
-                                    <div className="flex">
-                                        <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4">
-                                            <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
-                                        </svg>
-                                        <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
-                                            <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
-                                        </svg>
-                                        <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
-                                            <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
-                                        </svg>
-                                        <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
-                                            <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
-                                        </svg>
-                                        <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
-                                            <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
-                                        </svg>
+                            index < 9 ?
+                                <div key={index} className="mt-4 w-full h-[25.5rem] md:w-[30%] md:h-[20rem] md:mr-1 group">
+                                    <div className="w-full h-[80%] flex items-center justify-center overflow-hidden rounded-lg">
+                                        <img className="w-full h-full rounded-lg object-cover group-hover:scale-110 duration-700 transition-all" src={data.strMealThumb} alt="" />
                                     </div>
-                                    <p className="mt-1 text-lg font-semibold group-hover:text-[#ff642f]">{data.strMeal}</p>
-                                </div>
-                            </div>
+                                    <div className="w-full mt-2">
+                                        <div className="flex">
+                                            <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4">
+                                                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
+                                            </svg>
+                                            <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
+                                                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
+                                            </svg>
+                                            <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
+                                                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
+                                            </svg>
+                                            <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
+                                                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
+                                            </svg>
+                                            <svg xml-1ns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ff642f" className="size-4 ml-1">
+                                                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" />
+                                            </svg>
+                                        </div>
+                                        <p className="mt-1 text-lg font-semibold group-hover:text-[#ff642f]">{data.strMeal}</p>
+                                    </div>
+                                </div> : null
                         )
                     })
                 }
--- src/components/subscription/Subscription.tsx
+++ src/components/subscription/Subscription.tsx
@@ -1,8 +1,20 @@
 
 const Subscription = () => {
     return (
-        <div className="w-full h-[25rem] bg-[#ffd8ca] mt-8">
-
+        <div className="w-full h-[28rem] sm:h-[34rem] bg-[#ffd8ca] mt-8 flex flex-col items-center justify-center text-center">
+            <div className="w-full sm:w-[35rem] flex flex-col items-center justify-center text-center">
+                <h2 className="text-4xl sm:text-7xl font-semibold font-playfair-display mb-4">Deliciousness to your inbox</h2>
+                <p className="text-lg md:text-xl">Enjoy weekly hand picked recipes <br />and recommendations </p>
+                <div className="w-full sm:h-[4rem] sm:relative sm:flex sm:items-center sm:justify-end">
+                    <input type="email" className="w-[97%] h-12 sm:h-[4rem] rounded-sm placeholder:pl-4 placeholder-slate-500 mt-4" placeholder="Email Address" />
+                    <input type="button" value="JOIN" className="sm:mr-[0.15rem] sm:mt-[0.6rem] sm:absolute sm:top-0 sm:w-[26%] sm:h-[3.8rem] w-[97%] h-12 bg-[#ff642f] hover:bg-[#f1825df7] rounded-sm text-white text-lg font-semibold mt-2" />
+                </div>
+                <small className="mt-4">By joining our newsletter you agree to our
+                    <a href="#" className="block text-xs md:inline group">
+                        <u className="decoration-[#ff642f] group-hover:text-[#ff642f]"> Terms and Conditions</u>
+                    </a>
+                </small>
+            </div>
         </div>
     )
 }
