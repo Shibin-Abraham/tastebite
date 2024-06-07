8a058e32cf23ec0d7b4072f8ca4cdc8378f3467e
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-06-07 12:22:51 +0530

    api data displayed

--- src/components/Specialsection/HandPicked.tsx
+++ src/components/Specialsection/HandPicked.tsx
@@ -1,12 +1,43 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const HandPicked = () => {
-    return (
-        <div className="w-full h-auto">
-            <h5 className="text-2xl font-playfair-display font-semibold mt-16">Hand-Picked Collections</h5>
-            <div className="w-full h-[20rem] bg-red-300 mt-6">
-                <div>
+    type handPickedMeal = {
+        idMeal: string,
+        strMeal: string,
+        strMealThumb: string
 
-                </div>
+    }
+    const [handPicked, setHandPicked] = useState<handPickedMeal[]>([])
+
+    useEffect(() => {
+        const url = "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+        axios.get(url).then(response => {
+            setHandPicked(response.data.meals)
+        })
+    }, [])
+    return (
+        <div className="w-full h-auto flex flex-col items-center">
+            <h5 className="w-[457px] sm:w-full text-2xl font-playfair-display font-semibold mt-16 md:text-6xl">Hand-Picked Collections</h5>
+            <div className="w-[457px] mt-6 sm:w-full md:flex md:flex-wrap justify-between">
+                {
+                    handPicked.map((data) => {
+                        return (
+                            <div key={data.idMeal} className="w-full h-[29rem] md:w-[23rem] lg:w-[28rem] md:h-[23rem] lg:h-[30rem] xl:w-[32rem] rounded-md
+                             border-[1px] border-slate-300 flex flex-col items-center mt-4 md:mt-8 overflow-hidden">
+                                <img className="w-full h-[268px] object-cover" src={data.strMealThumb} alt="img" />
+                                <h5 className="text-4xl font-playfair-display font-semibold m-auto">{data.strMeal}</h5>
+                                <div className="w-full h-full flex items-center justify-end">
+                                    <span className="w-[8rem] border-[1px] border-black rounded-sm mr-6">
+                                        <p className="text-center font-medium">156 Recipes</p>
+                                    </span>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+
             </div>
         </div>
     )
