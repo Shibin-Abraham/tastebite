1fbfc8f7e895aadd03c08b03920e153e905a7cb8
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-06-15 23:12:21 +0530

    framer-motion edited

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,11 @@ import Subscription from "./components/subscription/Subscription"
 import HandPicked from "./components/Specialsection/HandPicked"
 import LatestRecipes from "./components/Specialsection/LatestRecipes"
 import Footer from "./components/footer/Footer"
-import { useEffect, useRef } from "react"
-import { motion, useInView, useAnimation } from "framer-motion"
-
 function App() {
-  const ref = useRef(null)
-  const isInView = useInView(ref, { once: true })
-
-  const mainControls = useAnimation()
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start("visible")
-    }
-  }, [])
 
   return (
-    <div ref={ref} className='bg-white max-w-screen h-full flex flex-col items-center justify-center overflow-y-scroll'>
-      <motion.div variants={{
-        hidden: { opacity: 0, y: 75 },
-        visible: { opacity: 1, y: 0 }
-      }}
-        initial="hidden"
-        animate={mainControls}
-        transition={{ duration: 0.5, delay: 0.25 }}
-        className="max-w-[95%] h-screen lg:w-[960px] xl:w-[1140px] 2xl:w-[1320px]">
+    <div className='bg-white max-w-screen h-full flex flex-col items-center justify-center overflow-y-scroll'>
+      <div className="max-w-[95%] h-screen lg:w-[960px] xl:w-[1140px] 2xl:w-[1320px]">
         <Header />
         <Main />
         <Category />
@@ -36,7 +17,7 @@ function App() {
         <HandPicked />
         <LatestRecipes />
         <Footer />
-      </motion.div>
+      </div>
     </div>
   )
 }
